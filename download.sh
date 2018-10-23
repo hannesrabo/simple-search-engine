16a53515b278f5e310696cc815c38d6d01463531
Hannes Rabo <hannes.rabo@gmail.com>
2018-10-23 14:11:47 +0200

    deleted old script

--- download.sh
+++
@@ -1,8 +0,0 @@
-
-mkdir data
-wget -P ./data "http://index.commoncrawl.org/CC-MAIN-2018-39-index?url=*.mc&output=json&page=0"
-wget -P ./data "http://index.commoncrawl.org/CC-MAIN-2018-39-index?url=*.mc&output=json&page=1"
-wget -P ./data "http://index.commoncrawl.org/CC-MAIN-2018-39-index?url=*.mc&output=json&page=2"
-wget -P ./data "http://index.commoncrawl.org/CC-MAIN-2018-39-index?url=*.mc&output=json&page=3"
-wget -P ./data "http://index.commoncrawl.org/CC-MAIN-2018-39-index?url=*.mc&output=json&page=4"
-wget -P ./data "http://index.commoncrawl.org/CC-MAIN-2018-39-index?url=*.mc&output=json&page=5"
